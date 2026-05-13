4bab49db3aff0bdd8fb05a603835d3e72d14baf2
Daisy <gracesukiwa@gmail.com>
2026-05-13 12:54:03 +0700

    disable build lint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['your-supabase-project.supabase.co'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
